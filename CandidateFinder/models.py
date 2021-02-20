54ecd14e8846f01b94b5d89446139643687bd69f
אביב בר-און <avivbaron7@gmail.com>
2021-02-20 21:33:58 +0200

    add models

--- CandidateFinder/models.py
+++ CandidateFinder/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    skill_name = models.CharField(max_length=50)
+
+class Job(models.Model):
+    job_name = models.CharField(max_length=50)
+    skill_id = models.ForeignKey(Skill, on_delete = models.CASCADE)
+
+class Candidate(models.Model):
+    candidate_name = models.CharField(max_length=50)
+    exp_years = models.PositiveIntegerField()
+    skill_id = models.ForeignKey(Skill, on_delete = models.CASCADE)
+    job_id = models.ForeignKey(Job, on_delete = models.CASCADE)
